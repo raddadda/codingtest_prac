192b7dda854de979eb17c56f2f07b146c2f3dc6f
raddadda <cyc123m@gmail.com>
2024-01-21 01:21:05 +0900

    240121

--- src/programers/p240117_1.java
+++ src/programers/p240117_1.java
@@ -1,5 +1,21 @@
 package programers;
 
 public class p240117_1 {
+	   	public int solution(int[] number) {
+	   		//삼총사
+	        int answer = 0;
+	        
+	        for(int i=0; i<number.length; i++){
+	            for(int j=i+1; j<number.length; j++){
+	                for(int k=j+1; k<number.length; k++){
+	                    if(number[i]+number[j]+number[k]==0){
+	                        answer++;
+	                    }
+	                }
+	            }
+	        }
+	        
+	        return answer;
+	    }
 
 }
--- src/programers/p240120.java
+++ src/programers/p240120.java
@@ -1,5 +1,28 @@
 package programers;
+import java.util.*;
 
 public class p240120 {
 
+	class Solution {
+	    public int solution(int k, int[] tangerine) {
+	        int answer = 0;
+	        
+	        int arr[] = new int[tangerine.length];
+	        
+	        for(int i=0; i<tangerine.length; i++){
+	            arr[tangerine[i]]++;
+	        }
+	        Arrays.sort(arr);
+	        
+	        int tmp=0;
+	        for(int i=tangerine.length-1; i>=0; i--){
+	            if(tmp>=k){
+	                break;
+	            }
+	            tmp+=arr[i];
+	            answer++;
+	        }
+	        return answer;
+	    }
+	}
 }
