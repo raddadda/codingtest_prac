8198e05010d9e1d0a57d082c8327f2792287c18f
raddadda <cyc123m@gmail.com>
2024-01-22 00:38:12 +0900

    240122

--- src/programers/p240122.java
+++ src/programers/p240122.java
@@ -1,5 +1,24 @@
 package programers;
-
+import java.util.*;
 public class p240122 {
+    public int solution(int[] elements) {
+    	//연속 부분 수열 합의 개수
+        int[] arr = new int[elements.length*2];
+        
+        for(int i=0; i<elements.length; i++){
+            arr[i] = arr[i+elements.length] = elements[i];
+        }
+        
+        Set<Integer> set = new HashSet<>();
+        
+        int[] arr2 = {1,2,3};
+        for(int i=1; i<=elements.length; i++){
+            for(int j=0; j<elements.length; j++){
+            	//Arrays.stream().sum() -> 배열 합 구하는 방
+                set.add(Arrays.stream(arr,j,j+i).sum());
+            }
+        }
 
+        return set.size();
+    }
 }
