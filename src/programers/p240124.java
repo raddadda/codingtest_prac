4ccf6be986f7f231659cfdb658039a46e33ec026
raddadda <cyc123m@gmail.com>
2024-01-25 23:56:17 +0900

    240125

---
+++ src/programers/p240124.java
@@ -0,0 +1,37 @@
+package programers;
+import java.util.*;
+public class p240124 {
+	//5->1 4->6 3->31
+	
+
+	class Solution {
+	    static String[] arr;
+	    static List<String> list;
+	    public int solution(String word) {
+	        int answer = 0;
+	        
+	        list = new ArrayList<>();
+	        arr = new String[]{"A","E","I","O","U"};
+	        recursion(word,"",0);
+	        
+	        for(int i=0; i<list.size(); i++){
+	            if(list.get(i).equals(word)){
+	                answer=i;
+	                break;
+	            }
+	        }
+	        
+	        return answer;
+	    }
+	    
+	    static void recursion(String word, String str,int depth){
+	        list.add(str);
+	        if(depth==5){
+	            return;
+	        }
+	        for(int i=0; i<arr.length; i++){
+	            recursion(word,str+arr[i],depth+1);
+	        }
+	    }
+	}
+} 
