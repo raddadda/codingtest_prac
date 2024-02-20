21080c9dfe04634e6943f6d4ff855c4eb652d31b
raddadda <cyc123m@gmail.com>
2024-02-20 23:15:49 +0900

    240220 backtracking

--- src/baekjoon/bj15652.java
+++ src/baekjoon/bj15652.java
@@ -5,15 +5,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class bj15651 {
-	static int N;
-	static int M;
+public class bj15652 {
 	static int[] arr;
 	static boolean[] visit;
+	static int N;
+	static int M;
 	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+		
 		
 		 N = Integer.parseInt(st.nextToken());
 		 M = Integer.parseInt(st.nextToken());
@@ -21,6 +22,7 @@ public class bj15651 {
 		visit = new boolean[N];
 		dfs(0);
 		System.out.print(sb);
+		
 	}
 	
 	static void dfs(int depth) {
@@ -32,13 +34,15 @@ public class bj15651 {
 			return;
 		}
 		
-		for(int i=0; i<N; i++) {
-			//if(!visit[i]){
-				visit[i]=true;
-				arr[depth] = i+1;
+		for(int i=0; i<N; i++){
+			//System.out.print("@"+i);
+			if(arr[depth-1]<=i) {
+				//System.out.print("!"+i);
+				visit[i]= true;
+				arr[depth]=i+1;
 				dfs(depth+1);
-				visit[i]=false;
-			//}
+				visit[i]= false;
+			}
 		}
 	}
 }
