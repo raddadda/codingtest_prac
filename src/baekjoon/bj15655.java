53ebf11e14afddeeda15c7b9ff16078a174c423c
raddadda <cyc123m@gmail.com>
2024-02-23 23:15:29 +0900

    240223 backtracking

---
+++ src/baekjoon/bj15655.java
@@ -0,0 +1,51 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class bj15655 {
+		static int N,M;
+		static int[] arr;
+		static int[] num;
+		static boolean[] visit;
+		static int n;
+		static StringBuilder sb = new StringBuilder();
+		public static void main(String[] args) throws IOException{
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			StringTokenizer st = new StringTokenizer(br.readLine());
+		
+			N = Integer.parseInt(st.nextToken());
+			M = Integer.parseInt(st.nextToken());
+			arr = new int[N];
+			num = new int[N];
+			visit = new boolean[N];
+			st = new StringTokenizer(br.readLine());
+			for(int i=0; i<N; i++) {
+				num[i] = Integer.parseInt(st.nextToken());
+			}
+			Arrays.sort(num);
+			dfs(0,0);
+			
+			System.out.print(sb);
+		}
+		static void dfs(int depth,int n) {
+			if(depth == M) {
+				for(int i=0; i<M; i++) {
+					sb.append(arr[i]).append(' ');
+				}
+				sb.append('\n');
+				return;
+			}
+			for(int i=n; i<N; i++) {
+				if(!visit[i]) {
+					visit[i] = true;
+					arr[depth] = num[i];
+					dfs(depth+1,i);
+					visit[i] = false;
+				}
+			}
+		}
+}
