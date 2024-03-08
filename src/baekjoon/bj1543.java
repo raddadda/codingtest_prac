d74a66d64800e2995dae1d79ea38f38cdfd50724
raddadda <cyc123m@gmail.com>
2024-03-08 23:57:06 +0900

    240308 greedy

---
+++ src/baekjoon/bj1543.java
@@ -0,0 +1,21 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class bj1543 {
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String n = br.readLine();
+		String n2 = br.readLine();
+		
+		int size = n.length();
+		int size2 = n2.length();
+		
+
+		n = n.replace(n2, "");
+		System.out.println((size - n.length()) / size2);
+	}
+
+}
