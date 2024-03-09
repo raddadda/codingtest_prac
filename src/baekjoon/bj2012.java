8b225ca362b4700a414cb5499e697fad74e9a4d1
raddadda <cyc123m@gmail.com>
2024-03-09 23:23:26 +0900

    240309 bj2012 brute force

---
+++ src/baekjoon/bj2012.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj2012 {
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int N = Integer.parseInt(st.nextToken());
+		for(int i=0; i<N; i++) {
+			int num = Integer.parseInt(st.nextToken());
+		}
+    }
+}
