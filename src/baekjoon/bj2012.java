5a152f0963b43e3396e68379b0c98d8c0c724079
raddadda <cyc123m@gmail.com>
2024-03-10 23:38:34 +0900

    240310 bj2012 greedy

--- src/baekjoon/bj2012.java
+++ src/baekjoon/bj2012.java
@@ -1,18 +1,29 @@
 package baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class bj2012 {
     public static void main(String[] args) throws IOException{
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
+	
+		int N = Integer.parseInt(br.readLine());
 		
-		int N = Integer.parseInt(st.nextToken());
-		for(int i=0; i<N; i++) {
-			int num = Integer.parseInt(st.nextToken());
+		int[] arr = new int[N+1];
+		
+		arr[0] = 0;
+		
+		for (int i=1; i<N+1; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
 		}
+		
+		Arrays.sort(arr);
+		
+		long answer = 0;
+		for (int i= 1; i<N+1; i++) { 
+			answer += Math.abs(i-arr[i]); 
+		}
+		System.out.println(answer);
     }
 }
