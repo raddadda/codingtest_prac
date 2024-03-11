ce545407f8b61b3ac3233cd145f941c63205f306
raddadda <cyc123m@gmail.com>
2024-03-12 00:09:10 +0900

    240312 level1

--- src/programers/p240311.java
+++ src/programers/p240311.java
@@ -3,12 +3,17 @@ package programers;
 public class p240311 {
 
 	public class Solution {
+		//자릿수 더하기
 	    public int solution(int n) {
 	        int answer = 0;
 
-	        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-	        System.out.println("Hello Java");
-
+	        while(true){
+	            answer+=n%10;
+	            if(n/10==0){
+	                break;
+	            }
+	            n=n/10;
+	        }
 	        return answer;
 	    }
 	}
