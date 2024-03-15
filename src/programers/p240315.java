f6f9219975ede2ad8a7cfcd72e7b1f3fd0e5c202
raddadda <cyc123m@gmail.com>
2024-03-15 23:49:03 +0900

    240315 level1

---
+++ src/programers/p240315.java
@@ -0,0 +1,19 @@
+package programers;
+
+public class p240315 {
+	class Solution {
+		//정수 제곱근 판별
+	    public long solution(long n) {
+	        long answer = 0;
+	        
+	        double sqrt = Math.sqrt(n);
+	        
+	        if(sqrt % 1 == 0) {
+	            answer = (long) Math.pow(sqrt + 1, 2);
+	        }
+	        else answer = -1;
+	        
+	        return answer;
+	    }
+	}
+}
