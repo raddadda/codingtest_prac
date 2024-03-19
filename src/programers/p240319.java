aa90f1021a3bf5667d7dc1d69244a12e433ec059
raddadda <cyc123m@gmail.com>
2024-03-19 23:56:39 +0900

    240319 level1

--- src/programers/p240319.java
+++ src/programers/p240319.java
@@ -2,4 +2,17 @@ package programers;
 
 public class p240319 {
 	//서울에서김서방찾기
+	class Solution {
+	    public String solution(String[] seoul) {
+	        StringBuilder sb = new StringBuilder();
+	        for(int i=0; i<seoul.length; i++){
+	            if(seoul[i].contains("Kim")){
+	                sb.append("김서방은 ").append(i).append("에 있다");
+	              
+	            }
+	        }
+	        
+	        return sb.toString();
+	    }
+	}
 }
