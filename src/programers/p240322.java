765237fe86f2a608292e721c4d6e718a78b36d91
raddadda <cyc123m@gmail.com>
2024-03-23 00:08:23 +0900

    240323 level _2

--- src/programers/p240322.java
+++ src/programers/p240322.java
@@ -4,11 +4,24 @@ public class p240322 {
 	
 	class Solution {
 	    public int[] solution(int[] arr, int divisor) {
+	    	//핸드폰 번호 가리기
 	        int[] answer = Arrays.stream(arr).filter(factor->factor%divisor==0).toArray();
 	        if(answer.length == 0){
 	            answer = new int[] {-1};
 	        }
 	        Arrays.sort(answer);
+	        
+	        
+	        /**
+	         *다른사람 풀이
+	         *toCharArray() 는 String을 char배열로 바꿔주는 메서
+			char[] ch = phone_number.toCharArray();
+			for(int i=0; i<ch.length-4; i++){
+				ch[i]='*';
+			}
+			return String.valueOf(ch);
+	         */
+	        
 	        return answer;
 	        
 	        
