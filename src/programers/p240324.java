d41bf66ae7aa9ada9bc16f46779f5821467dfb66
raddadda <cyc123m@gmail.com>
2024-03-24 23:38:39 +0900

    240324 level1

---
+++ src/programers/p240324.java
@@ -0,0 +1,14 @@
+package programers;
+
+public class p240324 {
+	class Solution {
+		//내적
+	    public int solution(int[] a, int[] b) {
+	        int answer = 0;
+	        for(int i=0; i<a.length; i++){
+	            answer+=a[i]*b[i];
+	        }
+	        return answer;
+	    }
+	}
+}
