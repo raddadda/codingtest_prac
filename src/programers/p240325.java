c91834207648718733dcea2bcb86a041128f9902
raddadda <cyc123m@gmail.com>
2024-03-25 23:59:06 +0900

    240325 level1

--- src/programers/p240325.java
+++ src/programers/p240325.java
@@ -4,7 +4,14 @@ public class p240325 {
 	class Solution {
 	    public String solution(int n) {
 	        String answer = "";
-	        
+	        String[] arr = {"수","박"};
+	        for(int i=0; i<n; i++){
+	            if(i%2==0){
+	                answer+="수";
+	            }else{
+	                answer+="박";
+	            }
+	        }
 	        return answer;
 	    }
 	}
