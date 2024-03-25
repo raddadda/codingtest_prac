ead8f5a91c4153512595d9c3247f8c6f4c95332f
raddadda <cyc123m@gmail.com>
2024-03-25 23:49:23 +0900

    240325 level1

---
+++ src/programers/p240325.java
@@ -0,0 +1,11 @@
+package programers;
+
+public class p240325 {
+	class Solution {
+	    public String solution(int n) {
+	        String answer = "";
+	        
+	        return answer;
+	    }
+	}
+}
