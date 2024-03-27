362efcb33bc3007899eac100d2ce049507442fb0
raddadda <cyc123m@gmail.com>
2024-03-27 11:48:28 +0900

    240327 level 1

--- src/programers/p240327_2.java
+++ src/programers/p240327_2.java
@@ -5,7 +5,9 @@ public class p240327_2 {
 	class Solution {
 	    public String solution(String s) {
 	        String answer = "";
+	        //문자열 내림차순으로 배치하기
 	        String[] str = s.split("");
+	        //내림차순으로 하는법Collections.reverseOrder
 	        Arrays.sort(str,Collections.reverseOrder());
 	        for(String a : str){
 	            answer += a;
