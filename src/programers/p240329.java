60d6498651917066f226753b29edae07bffd7c39
raddadda <cyc123m@gmail.com>
2024-03-30 00:21:37 +0900

    240330 level1

--- src/programers/p240329.java
+++ src/programers/p240329.java
@@ -4,24 +4,39 @@ public class p240329 {
 	class Solution {
 		//문자열 다루기 기본
 	    public boolean solution(String s) {
-	        boolean answer = true;
-	        chk(s);
-	        return answer;
-	    }
-	    
-	    static boolean chk(String s){
-	        
 	        if(s.length()!=4 && s.length()!=6){
 	                return false;
 	        }
+	        
 	        for(int i=0; i<s.length(); i++){
-	            if('0'<=s.charAt(i)-'0' && s.charAt(i)-'0'<='9'){
-	               return true;
+	            if('0'>s.charAt(i) || s.charAt(i)>'9'){
+	                return false;
 	            }
 	        }
-
+	        return true;
 	        
-	        return false;
+	        //다른 사람 풀이
+	        /**
+	         * 
+	         
+	        if(s.length() == 4 || s.length() == 6){
+	            try{
+	                int x = Integer.parseInt(s);
+	                return true;
+	            } catch(NumberFormatException e){
+	                return false;
+	            }
+	        }
+	        else return false;
+	         
+	         
+	       Integer.parseInt로 예외처리를 통해 푸는 방법
+	       NumberFormatException는 숫자형 포맷 오류 발생을 뜻한다.
+	       
+	         * 
+	         */
+
+
 	    }
 	}
 }
