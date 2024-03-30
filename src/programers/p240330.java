8c0fc795d2c02a1e83af7be48392384a6c9055f6
raddadda <cyc123m@gmail.com>
2024-03-30 23:52:37 +0900

    240330 level1

---
+++ src/programers/p240330.java
@@ -0,0 +1,16 @@
+package programers;
+
+public class p240330 {
+	class Solution {
+		//행렬의 덧셈
+	    public int[][] solution(int[][] arr1, int[][] arr2) {
+	        int[][] answer = new int[arr1.length][arr1[0].length];
+	        for(int i=0; i<arr1.length; i++){
+	            for(int j=0; j<arr1[0].length; j++){
+	               answer[i][j]=arr1[i][j]+arr2[i][j];
+	            }
+	        }
+	        return answer;
+	    }
+	}
+}
