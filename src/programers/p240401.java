2c56094d040a000edf89169b40c88dbab273f953
raddadda <cyc123m@gmail.com>
2024-04-02 00:09:06 +0900

    240402 level1

--- src/programers/p240401.java
+++ src/programers/p240401.java
@@ -2,29 +2,25 @@ package programers;
 
 public class p240401 {
 	class Solution {
-	    int gcd(int n, int m) {
-	        int r;
-	        while(m > 0) {
-	            r = n % m;
-	            n = m;
-	            m = r;
+	    //최대공약수와 최소공배수 구하는법
+	    static int d(int n,int m){
+	        //유클리드호제법 이용하기
+	        int r = 0;
+	        //n이 더 커야한다
+	        while(m>0){
+	            r=n%m;
+	            n=m;
+	            m=r;
 	        }
 	        return n;
 	    }
-	    
 	    public int[] solution(int n, int m) {
 	        int[] answer = new int[2];
-
-	        if(n < m) {
-	            int temp = n;
-	            n = m;
-	            m = temp;
-	        }
 	        
-	        answer[0] = gcd(n, m);
-	        
-	        answer[1] = n * m / answer[0];
-	        
+	        int left = Math.max(n,m);
+	        int right = Math.min(n,m);
+	        answer[0] = d(left,right);
+	        answer[1] = n*m/answer[0];
 	        return answer;
 	    }
 	}
