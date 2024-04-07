c57dbcc9414166a1097ea26f62dcb6b19175813c
raddadda <cyc123m@gmail.com>
2024-04-07 23:24:53 +0900

    240407 level 1

---
+++ src/programers/p240407.java
@@ -0,0 +1,23 @@
+package programers;
+
+public class p240407 {
+	class Solution {
+		//시저암호
+	    public String solution(String s, int n) {
+	        String answer = "";
+	        for(int i=0; i<s.length(); i++){
+	            char ch = s.charAt(i);
+	            
+	            if(Character.isLowerCase(ch)){
+	                ch = (char) ((ch-'a'+n)%26+'a');
+	            }else if(Character.isUpperCase(ch)){
+	                ch = (char) ((ch-'A'+n)%26+'A');
+	            }
+	            
+	            answer += ch;
+	        }
+	      
+	        return answer;
+	    }
+	}
+}
