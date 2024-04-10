7cbbcc67ce7ab4fd22ef0de988013dd0f27a4548
raddadda <cyc123m@gmail.com>
2024-04-11 00:03:19 +0900

    240411 level1

--- src/programers/p240410.java
+++ src/programers/p240410.java
@@ -1,5 +1,21 @@
 package programers;
 
 public class p240410 {
-
+	class Solution {
+		//하샤드 수
+	    public boolean solution(int x) {
+	        boolean answer = true;
+	        int sum=0;
+	        int num=x;
+	        while(num>0){
+	            sum+=num%10;
+	            num/=10;
+	        }
+	       
+	        if(x%sum!=0){
+	            answer=false;
+	        }
+	        return answer;
+	    }
+	}
 }
